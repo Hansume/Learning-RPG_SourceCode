af06a9b795e1c24c709b7bb91ac3cd95fc37d4cd
Trung Vu <trungquocvu1009@gmail.com>
2024-08-12 21:39:01 +0700

    Update RockSpawner.cs

--- ObjectPooling/RockSpawner.cs
+++ ObjectPooling/RockSpawner.cs
@@ -3,20 +3,14 @@ using UnityEngine;
 
 public class RockSpawner : MonoBehaviour
 {
-    bool isSpawning = false;
     private void Update()
     {
-        if (!isSpawning)
-        {
-            StartCoroutine(RockSpawning());
-        }
+        StartCoroutine(RockSpawning());
     }
 
     IEnumerator RockSpawning()
     {
-        isSpawning = true;
         yield return new WaitForSeconds(2f);
         ObjectPooling.Instance.SpawnFromPool("rock1", new Vector3(0, 15, 0), Quaternion.identity);
-        isSpawning = false;
     }
 }
